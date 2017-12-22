a65bb0709843f6c6ab2ce639b46c1ed2c60761f8
Broggoli <broggoli.nb@gmail.com>
2017-12-22 15:59:08 +0100

    Updated the volume_control code
    Added event detectors to the volume control GPIO pins and furthermore structured the code a bit better. Not jet debugged because I haven't got my raspberry Pi zero with me

--- music_player/controls/volume_control.py
+++ music_player/controls/volume_control.py
@@ -5,45 +5,65 @@ class Rotary_encoder(object):
     """
         Rotary_encoder is a class to control a rotary encoder
         for example a volume control.
+        Use for rotary encoder KY040.
+
+        Much of this code was 'inspired' by the code of martinohanlon on github:
+        https://github.com/martinohanlon/KY040/blob/master/ky040/KY040.py
     """
 
-    def __init__(self, clkGPIOpin, dtGPIOpin, swGPIOpin):
-        #encodes the rotation
-        self.clk = clkGPIOpin
-        self.dt = dtGPIOpin
+    def __init__(self, clockPin, dataPin, switchPin,
+                rotaryCallback=None, switchCallback=None,
+                rotaryBouncetime=250, switchBouncetime=300):
+        #These two GPIO Pins encodes the rotation
+        self.clk = clockPin
+        self.dt = dataPin
+        #This one checks for click events
+        self.sw = switchPin
 
-        #Checks for cklick events
-        self.sw = swGPIOpin
+        #Setting the callbacks
+        self.set_callbacks(rotaryCallback, switchCallback, rotaryBouncetime, switchBouncetime):
 
-        #set the GPIO pins up
+        #setting the GPIO pins up
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(self.dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.clk, GPIO.IN)
+        GPIO.setup(self.dt, GPIO.IN)
+        #The switch needs a pull up resistor in order to not float
         GPIO.setup(self.sw, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+        #setting the clk state for determining the direction of the turn
         self.clkLastState = GPIO.input(self.dt)
 
-    def check_direction(self):
-        """ returns 0 if nothing happened, 1 if clockwise turn, -1 if counterclockwise"""
-        clkState = GPIO.input(self.clk)
-        dtState = GPIO.input(self.dt)
-        #set the neutral direction to 0
-        direction = 0
+    def set_callbacks(self, rotaryCallback, switchCallback, rotaryBouncetime=250, switchBouncetime=300):
+        #Saving the callback functions that are called when an even occurs
+        self.rotaryCallback = rotaryCallback
+        self.switchCallback = switchCallback
+        #Setting the bounce time
+        self.rotaryBouncetime = rotaryBouncetime
+        self.switchBouncetime = switchBouncetime
 
-        if clkState != self.clkLastState:
+    def start(self):
+        #Adds event detectors to the clock and the switch pin
+        GPIO.add_event_detect(self.clockPin, GPIO.FALLING, callback=self._clockCallback, bouncetime=self.rotaryBouncetime)
+        GPIO.add_event_detect(self.switchPin, GPIO.FALLING, callback=self._switchCallback, bouncetime=self.switchBouncetime)
 
-            print("SW: ", GPIO.input(self.sw))
-            print("DT: ", GPIO.input(self.dt))
-            print("CLK: ", GPIO.input(self.clk))
+    def stop(self):
+        #removes the event detectors
+        GPIO.remove_event_detect(self.clockPin)
+        GPIO.remove_event_detect(self.switchPin)
 
-            # if there was an event set the direction to the corresponding value
-            if dtState != clkState:
-                    direction = 1
+    def _clockCallback(self):
+        #calls the callback function with the direction encoded as the question:
+        # is the rotation clockwise? -> True or False
+        if GPIO.input(self.clockPin) == 0:
+            data = GPIO.input(self.dataPin)
+            if data == 1:
+                self.rotaryCallback(False)
             else:
-                    direction = -1
+                self.rotaryCallback(True)
 
-        self.clkLastState = clkState
-        return direction
+    def _switchCallback(self):
+        if GPIO.input(self.switchPin) == 0:
+            self.switchCallback()
 
 class Volume_control(object):
     """
@@ -54,19 +74,24 @@ class Volume_control(object):
             min- & max_volume: pretty self explanatory
     """
 
-    def __init__(self, volume, min_volume=0, max_volume=100):
-        self.vol = volume
+    def __init__(self, initial_volume, play_pause, min_volume=0, max_volume=100):
+        self.volume = initial_volume
         self.minMaxVol = (min_volume, max_volume)
-        self.RE = Rotary_encoder(clkGPIOpin=27, dtGPIOpin=18, swGPIOpin=7)
+        self.RE = Rotary_encoder(clockPin=27, dataPin=18, switchPin=7,
+                                    self.adjust_volume, play_pause)
+    def adjust_volume(self, clockwise, adjustment_step = 1):
+        #If the direction is clockwise -> True -> increase Volume, else decrease Volume
 
-    def check_volume(self):
-        #If the direction is to the right: 1, left: -1, center: 0, adjust it
-
-        self.vol += self.RE.check_direction()
-
+        if clockwise:
+            self.volume += adjustment_step
+        else:
+            self.volume -= adjustment_step
 
         #keeping the volume in between min- and max_volume
-        self.vol = max(self.minMaxVol[0], self.vol)
-        self.vol = min(self.vol, self.minMaxVol[1])
+        self.volume = max(self.minMaxVol[0], self.volume)
+        self.volume = min(self.volume, self.minMaxVol[1])
 
-        return self.vol
+        return True
+
+    def get_volume(self):
+        return self.volume
