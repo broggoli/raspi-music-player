ae7c77a4afa8154291db2dd789247ca6189e332d
Broggoli <broggoli.nb@gmail.com>
2018-01-05 17:05:02 +0100

    Creating Playlists is now possible

--- music_player/list_manager.py
+++ music_player/list_manager.py
@@ -15,14 +15,16 @@ class List_Manager(object):
             tree = {0: "root"}
 
         dirList = os.listdir(directory)
+        firstSong = True
 
         for i, d in enumerate(dirList):
             if os.path.isdir(directory+"/"+d):
                 tree[d] = self.get_song_tree(directory+"/"+d, dirName=d)
             elif d.endswith(".mp3"):
-                if i == 0:
-                    tree = [dirName]
-                tree.append(d.split(".")[0])
+                if firstSong:
+                    tree["songs"] = [dirName]
+                    firstSong = False
+                tree["songs"].append(d.split(".")[0])
         return tree
 
 class Dir_List(List_Manager):
@@ -32,47 +34,54 @@ class Dir_List(List_Manager):
         self.rootDir = rootDir
         self.directory = directory
 
-        print(self.get_list())
-
     def get_list(self, tree=None, firstCall=True, found=False):
 
         if firstCall:
             tree = self.songTree
-
         if not found:
             for branch in tree:
                 if branch > 0:
                     if branch == self.directory:
                         return self.get_list(tree[branch], False, True)
-                    if not isinstance(tree[branch], list):
+
+                    if not "songs" in tree and len(tree) <= 2:
                         return self.get_list(tree[branch], False, False)
 
         else:
-            """print("found")
-            if not isinstance(tree, dict):
-                for branch in tree:
-                    return self.get_list(tree[branch], False, True)
-            else:"""
+            if tree == None:
+                print("No such Directory!")
             return tree
 
-dir_list = Dir_List("../../mp3", "playlist1")
+class Playlist():
 
-class Playlist(List_Manager):
-
-    def __init__(self, musicDir, playlistDir=None, shuffle = False):
-        super(Playlist, self).__init__(musicDir)
-        self.name = playlistDir
+    def __init__(self, rootDir, directory, shuffle = False):
+        self.dirList = Dir_List(rootDir, directory).get_list()
+        self.directory = rootDir
+        if self.dirList != None:
+            self.name = self.dirList[0]
+        else:
+            self.name = "Unknown"
         self.shuffle = shuffle
-        self.list = self.makePlaylist(playlistDir)
+        self.list = self.makePlaylist(self.dirList, self.directory)
 
-    def makePlaylist(self, playlistDir):
-        songList = []
-        for playlist in self.songTree:
-            if playlist != 0:
-                for song in self.songTree[playlist]:
-                    if song != 0 and song != "" and (self.songTree[playlist][0] == playlistDir or playlistDir == None):
-                            songPath = "%s/%s.mp3" %(playlist, self.songTree[playlist][song])
-                            songList.append(songPath)
-        if self.shuffle:
-            shuffle(songList)
-        return songList
+    def makePlaylist(self, l, path = ""):
+        """ returns a list of all the songs and a list wit hall the paths in the directory (also sub dirs) """
+        if l:
+            songList, pathList = [], []
+            for c, item in enumerate(l):
+                if c > 0:
+                    if item == "songs":
+                        i = [x for e,x in enumerate(l[item]) if e > 0]
+                        songList.extend(i)
+                        pathList.append(path+"/"+l[0])
+                    else:
+                        print(l[0])
+                        s, p = self.makePlaylist(l[item], path+"/"+l[0])
+                        songList.extend(s)
+                        pathList.extend(p)
+            return songList, pathList
+        else:
+            return None
+
+playlist = Playlist("../../mp3", "playlist1")
+print(playlist.list)
